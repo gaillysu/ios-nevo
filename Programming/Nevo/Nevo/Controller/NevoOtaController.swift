3ae0493d733f0a70afdb75be437bf8be4cfc59d1
supernova <gaillysu@139.com>
2015-02-04 09:24:12 +0800

    add NevoOtaController ,NevoOtaViewController class

---
+++ Programming/Nevo/Nevo/Controller/NevoOtaController.swift
@@ -0,0 +1,13 @@
+//
+//  NevoOtaController.swift
+//  Nevo
+//
+//  Created by supernova on 15/2/3.
+//  Copyright (c) 2015年 Nevo. All rights reserved.
+//
+
+import Cocoa
+
+class NevoOtaController: NSObject {
+
+}
