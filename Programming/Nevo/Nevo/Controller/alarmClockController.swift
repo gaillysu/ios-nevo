7f22d4e07278e9bb8b6ad5c0d87748756995e341
leiyuncun <546138263@qq.com>
2015-02-12 18:59:22 +0800

    no message

--- Programming/Nevo/Nevo/Controller/alarmClockController.swift
+++ Programming/Nevo/Nevo/Controller/alarmClockController.swift
@@ -8,17 +8,20 @@
 
 import UIKit
 
-class AlarmClockController: UIViewController, ConnectionControllerDelegate {
+class AlarmClockController: UIViewController, ConnectionControllerDelegate,alarmButtonActionCallBack {
+
+    @IBOutlet var alarmView: alarmClockView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        alarmView.bulidAlarmView(self)
         // Do any additional setup after loading the view.
     }
-    
+
     override func viewDidAppear(animated: Bool) {
         ConnectionControllerImpl.sharedInstance.setDelegate(self)
-        
+
         checkConnection()
     }
 
@@ -26,67 +29,92 @@ class AlarmClockController: UIViewController, ConnectionControllerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    func stringFromDate(date:NSDate) -> String {
+        var dateFormatter:NSDateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        let dateString:String = dateFormatter.stringFromDate(date)
+        return dateString
+    }
+
+    /*
+    call back Button Action
+    */
+    func controllManager(sender:AnyObject){
+
+        if sender.isEqual(alarmView.selectedTimerButton){
+            alarmView.initPickerView()
+            NSLog("alarmView.selectedTimerButton")
+        }
+
+        if sender.isEqual(alarmView.alarmSwitch){
+            NSLog("alarmView.alarmSwitch")
+        }
+
+        if sender.isEqual(alarmView.datePicker) {
+            let dateButtonTitle = stringFromDate(alarmView.datePicker.date)
+            alarmView.selectedTimerButton.setTitle(dateButtonTitle as String, forState: UIControlState.Normal)
+
+            NSLog("datePicker:%@",dateButtonTitle)
+        }
+    }
+    /**
+
+    See ConnectionControllerDelegate
+
+    */
+
+    func packetReceived(RawPacket) {
+
+        //Do nothing
+
+    }
+
+
 
     /**
-    
+
     See ConnectionControllerDelegate
-    
+
     */
-    
-    func packetReceived(RawPacket) {
-        
-        //Do nothing
-        
-    }
-    
-    
-    
-    /**
-    
-    See ConnectionControllerDelegate
-    
-    */
-    
+
     func connectionStateChanged(isConnected : Bool) {
-        
+
         //Maybe we just got disconnected, let's check
-        
+
         checkConnection()
-        
+
     }
-    
-    
-    
+
+
+
     /**
-    
+
     Checks if any device is currently connected
-    
+
     */
-    
+
     func checkConnection() {
-        
-        
-        
+
+
+
         if !ConnectionControllerImpl.sharedInstance.isConnected() {
-            
+
             //We are currently not connected
-            
-            
-            
+
+
+
             //TODO by Cloud Display the not connected screen instead of this popup
-            
+
             var alert:UIAlertView = UIAlertView(title:"Alert", message:"This is an example alert!", delegate:self, cancelButtonTitle:"Hide")
-            
-            
-            
+
+
+
             alert.show();
-            
-        } else {
-            //TODO by Cloud dismiss the popup
+
         }
-        
-        
+
+
     }
 
 }
