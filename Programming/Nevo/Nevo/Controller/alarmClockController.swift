68105b610474168c939ce80f0c500ab0c957548e
leiyuncun <546138263@qq.com>
2015-02-12 17:13:28 +0800

    add alarmViewController and view

---
+++ Programming/Nevo/Nevo/Controller/alarmClockController.swift
@@ -0,0 +1,35 @@
+//
+//  alarmClockController.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/2/12.
+//  Copyright (c) 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class alarmClockController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Programming/Nevo/Nevo/View/alarmClockView.swift
@@ -0,0 +1,144 @@
+//
+//  alarmClockView.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/2/12.
+//  Copyright (c) 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+/*
+StepGoalSetingView class all button events to follow this protocol
+*/
+protocol onActionCallBack {
+
+    func controllManager(sender:UIButton)
+    
+}
+
+class alarmClockView: UIView {
+
+    @IBOutlet var selectedTimerButton: UIButton!
+    @IBOutlet var alarmSwitch: UISwitch!
+
+    var datePicker:UIDatePicker!
+    let BAG_PICKER_TAG:Int = 1500;//Looking for view using a fixed tag values
+
+    let PICKER_BG_COLOR = UIColor(red: 186/255.0, green: 185/255.0, blue: 182/255.0, alpha: 1)//pickerView background color
+
+    let BUTTONBGVIEW_COLOR = UIColor(red: 244/255.0, green: 242/255.0, blue: 241/255.0, alpha: 1)//View button background color
+    var mDelegate:alarmClockController!
+
+    func bulidAlarmView(delegate:UIViewController) {
+        if let callBackDelgate = delegate as? alarmClockController {
+            mDelegate = callBackDelgate
+        }
+    }
+
+    @IBAction func controllEventManager(sender: AnyObject) {
+        
+    }
+
+    /*
+    Create a DatePicker
+    */
+    func initPickerView() {
+        //Create a DatePicker backgroundView
+        var PickerbgView:UIView = UIView(frame: CGRectMake(0, 210, self.frame.size.width, self.frame.size.height))
+        PickerbgView.backgroundColor = UIColor.clearColor()
+        PickerbgView.tag = BAG_PICKER_TAG
+        self.addSubview(PickerbgView)
+
+        //Create a DatePicker
+        datePicker = UIDatePicker(frame: CGRectMake(0, PickerbgView.frame.size.height-160-50, self.frame.size.width, 160))
+        datePicker.backgroundColor = PICKER_BG_COLOR
+        PickerbgView.addSubview(datePicker)
+
+        let buttonBgView:UIView = UIView(frame: CGRectMake(0, datePicker.frame.origin.y-40, datePicker.frame.size.width, 40))
+        buttonBgView.backgroundColor = BUTTONBGVIEW_COLOR
+        PickerbgView.addSubview(buttonBgView)
+
+        //Create a cancel button
+        let cancelButton:UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        cancelButton.frame = CGRectMake(0, datePicker.frame.origin.y-40, 50, 40)
+        cancelButton.setTitle("Cancel", forState: UIControlState.Normal)
+        cancelButton.backgroundColor = UIColor.clearColor()
+        cancelButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        cancelButton.addTarget(self, action: Selector("enterAction:"), forControlEvents: UIControlEvents.TouchUpInside)
+        PickerbgView.addSubview(cancelButton)
+
+        let enterButton:UIButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
+        enterButton.frame = CGRectMake(datePicker.frame.size.width-50, datePicker.frame.origin.y-40, 50, 40)
+        enterButton.setTitle("Enter", forState: UIControlState.Normal)
+        enterButton.backgroundColor = UIColor.clearColor()
+        enterButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        enterButton.addTarget(self, action: Selector("enterAction:"), forControlEvents: UIControlEvents.TouchUpInside)
+        PickerbgView.addSubview(enterButton)
+
+
+        //Create a click gesture
+        let tapCancel:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tapAction:")
+        PickerbgView.addGestureRecognizer(tapCancel)
+
+        //With animation will create the view
+        UIView.animateKeyframesWithDuration(0.3, delay: 0, options: UIViewKeyframeAnimationOptions.LayoutSubviews, animations: { () -> Void in
+            var frame:CGRect = PickerbgView.frame
+            frame.origin.y = 0;
+            PickerbgView.frame = frame;
+            }) { (Bool) -> Void in
+                
+        }
+        
+    }
+
+    /*
+    End pickerView performed operation
+    */
+    func EndAnimation() {
+        for view : AnyObject in self.subviews{
+            if view is UIView{
+                let bgPicker:UIView = view as UIView;
+                if bgPicker.tag == self.BAG_PICKER_TAG {
+
+                    UIView.animateKeyframesWithDuration(0.3, delay: 0, options: UIViewKeyframeAnimationOptions.LayoutSubviews, animations: { () -> Void in
+
+                        var frame:CGRect = bgPicker.frame
+                        frame.origin.y = 210;
+                        bgPicker.frame = frame;
+
+                        }) { (Bool) -> Void in
+
+                            bgPicker.removeFromSuperview()
+                    }
+
+                    
+                }
+            }
+        }
+        
+    }
+
+    /*
+    Click the cancelButton and enterButton events
+    */
+    func enterAction(sender:UIButton) {
+        EndAnimation()
+    }
+
+    /*
+    Click the tapGesture events
+    */
+    func tapAction(sender:UITapGestureRecognizer) {
+        EndAnimation()
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
