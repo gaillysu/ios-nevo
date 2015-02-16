af9070f99cd628eaff167e8d78fbf1874dad9486
leiyuncun <546138263@qq.com>
2015-02-16 17:34:20 +0800

    add custom Tabbar

--- Programming/Nevo/Nevo/Controller/MainTabBarController.swift
+++ Programming/Nevo/Nevo/Controller/MainTabBarController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class MainTabBarController: UITabBarController {
+class MainTabBarController: UITabBarController,UITabBarControllerDelegate {
+
+    var items:NSMutableArray!
+    var selectedItem:UIButton!
+    let FONT_RALEWAY_BOLD:UIFont! = UIFont(name:"Raleway-Thin", size: 23);
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.delegate = self
 
         // Do any additional setup after loading the view.
         var nav:UINavigationController!
@@ -33,13 +39,64 @@ class MainTabBarController: UITabBarController {
             }
 
         }
+        customInitTabbar()
+    }
+
+    func customInitTabbar(){
+        self.tabBar.hidden = true
+
+        //创建自定义tabbar  [UIScreen mainScreen].bounds.size.width
+        let myTabbarView:UIView = UIView(frame: CGRectMake(-1, self.view.frame.size.height-54, UIScreen.mainScreen().bounds.size.width+2, 55));
+        myTabbarView.backgroundColor = UIColor.blackColor();
+        myTabbarView.layer.borderWidth = 1.0;
+        myTabbarView.layer.borderColor = UIColor.grayColor().CGColor;
+        self.view.addSubview(myTabbarView)
+        if (items == nil) {
+            items = NSMutableArray(capacity: 5)
+        }
+        let imgArray:NSArray = NSArray(arrayLiteral: "selectedGoalitem","selectedHomeitem","selectedAlarmitem")
+        let selectImgArray:NSArray = NSArray(arrayLiteral: "goalitem","homeitem","alarmitem")
+
+        for (var i:Int = 0; i < imgArray.count; i++) {
+
+            var item:UIButton  = UIButton(frame:  CGRectMake(UIScreen.mainScreen().bounds.size.width/CGFloat(imgArray.count)*CGFloat(i), 0, UIScreen.mainScreen().bounds.size.width / CGFloat(imgArray.count), 55))
+            item.backgroundColor = UIColor.clearColor()
+            item.setImage(UIImage(named: selectImgArray[i] as String), forState: UIControlState.Selected)
+            item.setImage(UIImage(named: imgArray[i] as String), forState: UIControlState.Normal)
+            
+            if (i==1) {
+                item.selected=true
+                selectedItem=item
+            }
+            item.addTarget(self, action: Selector("tabbarItemClickAction:"), forControlEvents: UIControlEvents.TouchUpInside)
+            myTabbarView.addSubview(item)
+            item.tag = i;
+            items.addObject(item)
+
+        }
+
+
+    }
+
+    func tabbarItemClickAction(item:UIButton) {
+        item.selected = true
+        if ((selectedItem) != nil && !item.isEqual(selectedItem)) {
+            selectedItem.selected = false
+        }
+        selectedItem = item;
+
+        self.selectedIndex = item.tag;
+    }
+
+    func tabBarController(tabBarController: UITabBarController, didSelectViewController viewController: UIViewController){
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
 
     /*
     // MARK: - Navigation
