0406be0a9de6ce211d2033224f0d892a26223433
leiyuncun <546138263@qq.com>
2015-03-03 16:11:50 +0800

    fix Notification titleView

--- Programming/Nevo/Nevo/Controller/NotificationController.swift
+++ Programming/Nevo/Nevo/Controller/NotificationController.swift
@@ -10,26 +10,63 @@ import UIKit
 
 class NotificationController: UIViewController {
 
+
+    @IBOutlet var notificationList: NotificationView!
+    var listArray:NSArray = ["FaceBook","SMS","CALL","EMAIL"]
+    var typeInt:Int!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        var titleLabel:UILabel = UILabel(frame: CGRectMake(0, 0, 120, 30))
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.text = NSLocalizedString("Notification", comment: "")
+        titleLabel.font = UIFont.systemFontOfSize(25)
+        titleLabel.textAlignment = NSTextAlignment.Center
+        self.navigationItem.titleView = titleLabel
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
+    // MARK: - UITableViewDelegate
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+
+        return 43.0
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+        typeInt = indexPath.row
+        self.performSegueWithIdentifier("EnterNotification", sender: self)
+    }
+
+    // MARK: - UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+
+        return listArray.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = notificationList.NotificationlistCell(indexPath, dataSource: listArray)
+        return cell
+    }
+
+
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if (segue.identifier == "EnterNotification"){
+            NSLog("segue.identifier\(segue.identifier)")
+            var notficp = segue.destinationViewController as EnterNotificationController
+            notficp.type = typeInt
+
+        }
+
     }
-    */
 
 }
