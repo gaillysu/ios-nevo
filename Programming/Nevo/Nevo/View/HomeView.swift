b91f39762e652a5c3754318b3b6edfff5bfa3667
ideas <ideas@ideastekiMac-mini.local>
2015-03-03 11:12:45 +0800

    Add a progress bar in home view page

--- Programming/Nevo/Nevo/View/HomeView.swift
+++ Programming/Nevo/Nevo/View/HomeView.swift
@@ -14,11 +14,17 @@ class HomeView: UIView {
     //Put all UI operation HomeView inside
     private let mClockTimerView = ClockView(frame:CGRectMake(0, 0, UIScreen.mainScreen().bounds.width-60, UIScreen.mainScreen().bounds.width-60), hourImage:  UIImage(named: "clockViewHour")!, minuteImage: UIImage(named: "clockViewMinute")!, dialImage: UIImage(named: "clockView600")!);//init "ClockView" ,Use the code relative layout
 
+    let mProgressView:UIProgressView = UIProgressView(frame: CGRectMake(UIScreen.mainScreen().bounds.width/4, UIScreen.mainScreen().bounds.height-80, UIScreen.mainScreen().bounds.width/2, 20))
+    
     func bulidHomeView() {
 
         mClockTimerView.currentTimer()
         self.addSubview(mClockTimerView)
         mClockTimerView.center = CGPointMake(self.frame.width/2.0, self.frame.height/2.0);//Using the center property determines the location of the ClockView
+        //add the progressbar
+        mProgressView.progressTintColor = AppTheme.NEVO_SOLAR_YELLOW()
+        mProgressView.setProgress(0.57, animated: true)
+        self.addSubview(mProgressView)
     }
     
     func getClockTimerView() -> ClockView {
