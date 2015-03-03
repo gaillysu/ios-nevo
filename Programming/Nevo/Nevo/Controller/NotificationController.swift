bacd3170a5f9e93cfd5727d0a04939add13793a5
leiyuncun <546138263@qq.com>
2015-03-03 16:06:04 +0800

    add image

---
+++ Programming/Nevo/Nevo/Controller/NotificationController.swift
@@ -0,0 +1,35 @@
+//
+//  NotificationController.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/3/3.
+//  Copyright (c) 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class NotificationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
