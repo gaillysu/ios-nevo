fd6835058d9652ea57bfdcd1e92259c1727507aa
leiyuncun <546138263@qq.com>
2015-03-05 17:28:51 +0800

    add the rotation animation class

---
+++ Programming/Nevo/Nevo/View/AnimationView.swift
@@ -0,0 +1,21 @@
+//
+//  AnimationView.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/3/5.
+//  Copyright (c) 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class AnimationView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
