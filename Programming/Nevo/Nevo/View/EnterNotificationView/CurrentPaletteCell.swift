798f3a49d911df94d9c102b624f5e5daf2d005bd
leiyuncun <546138263@qq.com>
2015-03-11 10:14:20 +0800

    add file

---
+++ Programming/Nevo/Nevo/View/EnterNotificationView/CurrentPaletteCell.swift
@@ -0,0 +1,24 @@
+//
+//  CurrentPaletteCell.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/3/10.
+//  Copyright (c) 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class CurrentPaletteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
