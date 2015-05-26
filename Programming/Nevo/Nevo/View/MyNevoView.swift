537604c6de997065679bba36b3486e604365bbcd
leiyuncun <546138263@qq.com>
2015-05-26 14:41:46 +0800

    fix resources image

---
+++ Programming/Nevo/Nevo/View/MyNevoView.swift
@@ -0,0 +1,13 @@
+//
+//  MyNevoView.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/5/25.
+//  Copyright (c) 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class MyNevoView: UIVivew {
+   
+}
