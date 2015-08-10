74ba6a8c7980545c366c8cac3c5765c526cc46e4
leiyuncun <546138263@qq.com>
2015-08-10 16:55:19 +0800

    add the writter to a local database class

---
+++ Programming/Nevo/Nevo/Model/DBModel/DaySleepSaveModel.swift
@@ -0,0 +1,21 @@
+//
+//  DaySleepSaveModel.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/8/10.
+//  Copyright (c) 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class DaySleepSaveModel: NevoDBModel {
+    var sleepTime:Int?
+    var wakeSleepTime:Int?
+    var lightSleepTime:Int?
+    var deepSleepTime:Int?
+
+    override init() {
+
+    }
+
+}
