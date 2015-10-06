a45647932bd17d39acd28a42d1a19a276d825f86
leiyuncun <546138263@qq.com>
2015-10-06 14:04:54 +0800

    add cocoapods management tools

---
+++ Programming/Nevo/Nevo/View/SleepTrackingView.swift
@@ -0,0 +1,21 @@
+//
+//  SleepTrackingView.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/10/6.
+//  Copyright © 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class SleepTrackingView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
