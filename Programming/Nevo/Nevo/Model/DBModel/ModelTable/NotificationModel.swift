7ba229d7ee28707d9be22255e787d103b4d7d907
leiyuncun <546138263@qq.com>
2015-12-16 18:40:22 +0800

    [][add the notification icon resources]

---
+++ Programming/Nevo/Nevo/Model/DBModel/ModelTable/NotificationModel.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationModel.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/12/16.
+//  Copyright © 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class NotificationModel: NSObject {
+
+}
---
+++ Programming/Nevo/Nevo/Model/DBModel/ModelTable/UserNotification.swift
@@ -0,0 +1,13 @@
+//
+//  UserNotification.swift
+//  Nevo
+//
+//  Created by leiyuncun on 15/12/16.
+//  Copyright © 2015年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class UserNotification: NSObject {
+
+}
