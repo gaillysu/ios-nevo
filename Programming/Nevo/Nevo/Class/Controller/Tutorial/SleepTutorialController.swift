f1997a75344596b4999d38c01038309442c70bec
leiyuncun <546138263@qq.com>
2016-02-29 14:34:08 +0800

    [add class][add tutorial class]

---
+++ Programming/Nevo/Nevo/Class/Controller/Tutorial/SleepTutorialController.swift
@@ -0,0 +1,52 @@
+//
+//  SleepTutorialController.swift
+//  Nevo
+//
+//  Created by leiyuncun on 16/2/29.
+//  Copyright © 2016年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class SleepTutorialController: UIViewController {
+
+    init() {
+        super.init(nibName: "SleepTutorialController", bundle: NSBundle.mainBundle())
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonManager(sender: AnyObject) {
+        let nav:MorningTutorialController = MorningTutorialController()
+        self.presentViewController(nav, animated: true) { () -> Void in
+
+        }
+//        self.dismissViewControllerAnimated(true) { () -> Void in
+//            
+//        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
