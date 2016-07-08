70167a118e5787c9e3c6ff481ac6d4462cdfd072
leiyuncun <546138263@qq.com>
2016-07-08 16:19:36 +0800

    Implement lunar login

--- Programming/Nevo/LunaR/Class/Controller/LoginController/LoginController.swift
+++ Programming/Nevo/LunaR/Class/Controller/LoginController/LoginController.swift
@@ -13,9 +13,14 @@ import BRYXBanner
 import XCGLogger
 import LTNavigationBar
 import UIColor_Hex_Swift
+import AutocompleteField
 
 class LoginController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var userNameTextField: AutocompleteField!
+    @IBOutlet weak var passwordTextField: AutocompleteField!
+    @IBOutlet weak var logoinButton: UIButton!
+    
     var userName:String = ""
     var password:String = ""
 
@@ -51,9 +56,13 @@ class LoginController: UIViewController,UITextFieldDelegate {
         
     }
 
-    @IBAction func registerButtonAction(sender: AnyObject) {
-        let registerController =  RegisterController()
-        self.navigationController?.pushViewController(registerController, animated: true);
+    @IBAction func buttonAction(sender: AnyObject) {
+        if sender.isEqual(logoinButton) {
+            self.loginRequest()
+        }else{
+            let registerController =  RegisterController()
+            self.navigationController?.pushViewController(registerController, animated: true);
+        }
     }
     
     func delay(delay:Double, closure:()->()) {
@@ -70,71 +79,71 @@ class LoginController: UIViewController,UITextFieldDelegate {
     }
     
     
-//    func loginRequest() {
-//        let usercell:SetingLoginCell = (logoinTableview.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0))!) as! SetingLoginCell
-//            userName = usercell.userEmail!.text!
-//            password = usercell.userPassword!.text!
-//        if AppDelegate.getAppDelegate().network!.isReachable {
-//            XCGLogger.defaultInstance().debug("有网络")
-//            if(AppTheme.isNull(userName) || !AppTheme.isEmail(userName)) {
-//                let banner = Banner(title: NSLocalizedString("Email is not filled in.", comment: ""), subtitle: nil, image: nil, backgroundColor:UIColor.getBaseColor())
-//                banner.dismissesOnTap = true
-//                banner.show(duration: 1.2)
-//                return
-//            }
-//            
-//            if AppTheme.isNull(password) || AppTheme.isPassword(password) {
-//                let banner = Banner(title: NSLocalizedString("Password is not filled in.", comment: ""), subtitle: nil, image: nil, backgroundColor:UIColor.getBaseColor())
-//                banner.dismissesOnTap = true
-//                banner.show(duration: 1.2)
-//                return
-//            }
-//            
-//            let view = MRProgressOverlayView.showOverlayAddedTo(self.navigationController!.view, title: "Please wait...", mode: MRProgressOverlayViewMode.Indeterminate, animated: true)
-//            view.setTintColor(UIColor.getBaseColor())
-//
-//            
-//            HttpPostRequest.postRequest("http://nevo.karljohnchow.com/user/login", data: ["user":["email":userName,"password":password]]) { (result) in
-//                MRProgressOverlayView.dismissAllOverlaysForView(self.navigationController!.view, animated: true)
-//                
-//                let json = JSON(result)
-//                let message = json["message"].stringValue.isEmpty ? NSLocalizedString("not_login", comment: ""):json["message"].stringValue
-//                let status = json["status"].intValue
-//                
-//                let banner = Banner(title: NSLocalizedString(message, comment: ""), subtitle: nil, image: nil, backgroundColor: status > 0 ? UIColor.getBaseColor():UIColor.getBaseColor())
-//                banner.dismissesOnTap = true
-//                banner.show(duration: 1.2)
-//                
-//                //status > 0 login success or login fail
-//                if(status > 0 && UserProfile.getAll().count == 0) {
-//                    let user = json["user"]
-//                    let jsonBirthday = user["birthday"];
-//                    let dateString: String = jsonBirthday["date"].stringValue
-//                    var birthday:String = ""
-//                    if !jsonBirthday.isEmpty || !dateString.isEmpty {
-//                        let dateFormatter = NSDateFormatter()
-//                        dateFormatter.dateFormat = "y-M-d h:m:s.000000"
-//                        
-//                        let birthdayDate = dateFormatter.dateFromString(dateString)
-//                        dateFormatter.dateFormat = "y-M-d"
-//                        birthday = dateFormatter.stringFromDate(birthdayDate!)
-//                    }
-//                    
-//                    let userprofile:UserProfile = UserProfile(keyDict: ["id":user["id"].intValue,"first_name":user["first_name"].stringValue,"last_name":user["last_name"].stringValue,"birthday":birthday,"length":user["length"].intValue,"email":user["email"].stringValue, "weight":user["weight"].floatValue])
-//                    userprofile.add({ (id, completion) in
-//                        XCGLogger.defaultInstance().debug("Added? id = \(id)")
-//                    })
-//                    self.navigationController?.popViewControllerAnimated(true)
-//                }
-//               
-//            }
-//        }else{
-//            
-//            XCGLogger.defaultInstance().debug("没有网络")
-//            let view = MRProgressOverlayView.showOverlayAddedTo(self.navigationController!.view, title: "No internet", mode: MRProgressOverlayViewMode.Cross, animated: true)
-//            view.setTintColor(UIColor.getBaseColor())
-//        }
-//        
-//    }
+    func loginRequest() {
+        
+            userName = userNameTextField!.text!
+            password = passwordTextField!.text!
+        if AppDelegate.getAppDelegate().network!.isReachable {
+            XCGLogger.defaultInstance().debug("有网络")
+            if(AppTheme.isNull(userName) || !AppTheme.isEmail(userName)) {
+                let banner = Banner(title: NSLocalizedString("Email is not filled in.", comment: ""), subtitle: nil, image: nil, backgroundColor:UIColor.getBaseColor())
+                banner.dismissesOnTap = true
+                banner.show(duration: 1.2)
+                return
+            }
+            
+            if AppTheme.isNull(password) || AppTheme.isPassword(password) {
+                let banner = Banner(title: NSLocalizedString("Password is not filled in.", comment: ""), subtitle: nil, image: nil, backgroundColor:UIColor.getBaseColor())
+                banner.dismissesOnTap = true
+                banner.show(duration: 1.2)
+                return
+            }
+            
+            let view = MRProgressOverlayView.showOverlayAddedTo(self.navigationController!.view, title: "Please wait...", mode: MRProgressOverlayViewMode.Indeterminate, animated: true)
+            view.setTintColor(UIColor.getBaseColor())
+
+            
+            HttpPostRequest.postRequest("http://nevo.karljohnchow.com/user/login", data: ["user":["email":userName,"password":password]]) { (result) in
+                MRProgressOverlayView.dismissAllOverlaysForView(self.navigationController!.view, animated: true)
+                
+                let json = JSON(result)
+                let message = json["message"].stringValue.isEmpty ? NSLocalizedString("not_login", comment: ""):json["message"].stringValue
+                let status = json["status"].intValue
+                
+                let banner = Banner(title: NSLocalizedString(message, comment: ""), subtitle: nil, image: nil, backgroundColor: status > 0 ? UIColor.getBaseColor():UIColor.getBaseColor())
+                banner.dismissesOnTap = true
+                banner.show(duration: 1.2)
+                
+                //status > 0 login success or login fail
+                if(status > 0 && UserProfile.getAll().count == 0) {
+                    let user = json["user"]
+                    let jsonBirthday = user["birthday"];
+                    let dateString: String = jsonBirthday["date"].stringValue
+                    var birthday:String = ""
+                    if !jsonBirthday.isEmpty || !dateString.isEmpty {
+                        let dateFormatter = NSDateFormatter()
+                        dateFormatter.dateFormat = "y-M-d h:m:s.000000"
+                        
+                        let birthdayDate = dateFormatter.dateFromString(dateString)
+                        dateFormatter.dateFormat = "y-M-d"
+                        birthday = dateFormatter.stringFromDate(birthdayDate!)
+                    }
+                    
+                    let userprofile:UserProfile = UserProfile(keyDict: ["id":user["id"].intValue,"first_name":user["first_name"].stringValue,"last_name":user["last_name"].stringValue,"birthday":birthday,"length":user["length"].intValue,"email":user["email"].stringValue, "weight":user["weight"].floatValue])
+                    userprofile.add({ (id, completion) in
+                        XCGLogger.defaultInstance().debug("Added? id = \(id)")
+                    })
+                    self.navigationController?.popViewControllerAnimated(true)
+                }
+               
+            }
+        }else{
+            
+            XCGLogger.defaultInstance().debug("没有网络")
+            let view = MRProgressOverlayView.showOverlayAddedTo(self.navigationController!.view, title: "No internet", mode: MRProgressOverlayViewMode.Cross, animated: true)
+            view.setTintColor(UIColor.getBaseColor())
+        }
+        
+    }
 
 }
