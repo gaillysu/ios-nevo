04860e52ee5d81db064c56b0f38ce99cd3083fe9
leiyuncun <546138263@qq.com>
2016-08-09 14:48:13 +0800

    fix solar ui test data

--- Programming/Nevo/LunaR/Class/Controller/SolarIndicator/SolarIndicatorController.swift
+++ Programming/Nevo/LunaR/Class/Controller/SolarIndicator/SolarIndicatorController.swift
@@ -13,6 +13,8 @@ class SolarIndicatorController: PublicClassController {
 
     @IBOutlet weak var textCollection: UICollectionView!
     @IBOutlet weak var pieChartView: PieChartView!
+    private var onTitle:[String] = ["Timer on Battery","Timer on Solar"]
+    private var onValue:[Double] = [130,250]
     
     init() {
         super.init(nibName: "SolarIndicatorController", bundle: NSBundle.mainBundle())
@@ -24,12 +26,20 @@ class SolarIndicatorController: PublicClassController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        textCollection.registerNib(UINib(nibName: "SolarInforViewCell",bundle: nil), forCellWithReuseIdentifier: "SolarInfor_Identifier")
+        
         self.setupPieChartView(pieChartView)
+        pieChartView.legend.enabled = false;
         pieChartView.delegate = self
         self.updateChartData()
         pieChartView.animate(xAxisDuration: 1.4, easingOption: ChartEasingOption.EaseOutBack)
+        pieChartView.backgroundColor = UIColor.getGreyColor()
 
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.view.backgroundColor = UIColor.getGreyColor()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,6 +47,31 @@ class SolarIndicatorController: PublicClassController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+// MARK: - UICollectionViewDelegate
+extension SolarIndicatorController:UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        return CGSizeMake(collectionView.frame.size.width, 40)
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return onTitle.count
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("SolarInfor_Identifier", forIndexPath: indexPath)
+        cell.backgroundColor = UIColor.clearColor()
+        (cell as! SolarInforViewCell).titleLabel.text = onTitle[indexPath.row]
+        if onValue.count>0 {
+            (cell as! SolarInforViewCell).valueLabel.text = String(format: "%dh %dmin", Int(onValue[indexPath.row])/60,Int(onValue[indexPath.row])%60)
+        }
+        return cell
+    }
 }
 
 // MARK: - ChartViewDelegate
@@ -47,10 +82,11 @@ extension SolarIndicatorController:ChartViewDelegate {
     }
 
     func setupPieChartView(chartView:PieChartView) {
+        
         chartView.usePercentValuesEnabled = true
         chartView.drawSlicesUnderHoleEnabled = false
-        chartView.holeRadiusPercent = 0.58
-        chartView.transparentCircleRadiusPercent = 0.61
+        chartView.holeRadiusPercent = 0.0
+        chartView.transparentCircleRadiusPercent = 0.0
         chartView.descriptionText = ""
         chartView.setExtraOffsets(left: 5.0, top: 10.0, right: 5.0, bottom: 5.0)
         chartView.drawCenterTextEnabled = true
@@ -62,7 +98,11 @@ extension SolarIndicatorController:ChartViewDelegate {
         centerText.setAttributes([NSFontAttributeName:UIFont(name: "HelveticaNeue-Light", size: 13.0)!], range: NSMakeRange(0, centerText.length))
         centerText.addAttributes([NSFontAttributeName:UIFont(name: "HelveticaNeue-Light", size: 11.0)!,NSForegroundColorAttributeName:UIColor.grayColor()], range: NSMakeRange(10, centerText.length - 10))
         centerText.addAttributes([NSFontAttributeName:UIFont(name: "HelveticaNeue-LightItalic", size: 11.0)!,NSForegroundColorAttributeName:UIColor(red: 51.0/255.0, green: 181.0/255.0, blue: 229.0/255.0, alpha: 1.0)], range: NSMakeRange(centerText.length - 19, 19))
+        //let marker:BalloonMarker = BalloonMarker(color: UIColor.getBaseColor(), font: UIFont(name: "Helvetica-Light", size: 11)!, insets: UIEdgeInsetsMake(8.0, 8.0, 15.0, 8.0))
+        //marker.minimumSize = CGSizeMake(60, 25);
+        //chartView.marker = marker
         chartView.centerAttributedText = centerText
+        chartView.drawCenterTextEnabled = false
         chartView.drawHoleEnabled = true;
         chartView.rotationAngle = 0.0;
         chartView.rotationEnabled = true;
@@ -80,7 +120,8 @@ extension SolarIndicatorController:ChartViewDelegate {
         let mult:Double = range
         var yVals1:[ChartDataEntry] = []
         for i:Int in 0..<count {
-            yVals1.append(BarChartDataEntry(value: Double(arc4random_uniform(UInt32(mult) + UInt32(mult/5))), xIndex: i))
+            let value:Double = Double(arc4random_uniform(UInt32(mult) + UInt32(mult/5)))
+            yVals1.append(BarChartDataEntry(value: value , xIndex: i))
         }
         
         var xVals:[String] = []
@@ -92,43 +133,22 @@ extension SolarIndicatorController:ChartViewDelegate {
         let dataSet:PieChartDataSet = PieChartDataSet(yVals: yVals1, label: "Election Results")
         dataSet.sliceSpace = 2.0;
         var colors:[UIColor] = [];
-        for color in ChartColorTemplates.vordiplom() {
-            colors.append(color)
-        }
-        
-        for color in ChartColorTemplates.joyful() {
-            colors.append(color)
-        }
-        
-        for color in ChartColorTemplates.colorful() {
-            colors.append(color)
-        }
-        
-        for color in ChartColorTemplates.liberty() {
-            colors.append(color)
-        }
-        
-        for color in ChartColorTemplates.pastel() {
-            colors.append(color)
-        }
-        
-        for color in ChartColorTemplates.pastel() {
-            colors.append(color)
-        }
-        
-        colors.append(UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1))
+        colors.append(UIColor(red: 179/255, green: 126/255, blue: 189/255, alpha: 1))
+        colors.append(UIColor.getBaseColor())
         dataSet.colors = colors
         
         let data:PieChartData = PieChartData(xVals: xVals, dataSets: [dataSet])
+        //data.highlightEnabled = false
         let pFormatter:NSNumberFormatter = NSNumberFormatter()
         pFormatter.numberStyle = NSNumberFormatterStyle.PercentStyle;
         pFormatter.maximumFractionDigits = 1;
         pFormatter.multiplier = 1.0;
         pFormatter.percentSymbol = " %";
         
+        
         data.setValueFormatter(pFormatter)
         data.setValueFont(UIFont(name: "HelveticaNeue-Light", size: 11.0))
-        data.setValueTextColor(UIColor.whiteColor())
+        data.setValueTextColor(UIColor.blackColor())
         pieChartView.data = data;
         pieChartView.highlightValues(nil)
     }
---
+++ Programming/Nevo/LunaR/Class/Controller/SolarIndicator/SolarInforViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SolarInforViewCell.swift
+//  Nevo
+//
+//  Created by leiyuncun on 16/8/9.
+//  Copyright © 2016年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+class SolarInforViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
