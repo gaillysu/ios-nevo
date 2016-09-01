bd769f2e7c4f84753cd4d10209e7d93128f836d5
leiyuncun <546138263@qq.com>
2016-09-01 11:37:55 +0800

    add selected header image

--- Programming/Nevo/Nevo/Class/Controller/UserProfileController/UserHeader.swift
+++ Programming/Nevo/Nevo/Class/Controller/UserProfileController/UserHeader.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class UserHeader: UITableViewHeaderFooterView {
 
+    @IBAction func uploadAction(sender: AnyObject) {
+        let alert:UIAlertController = UIAlertController(title: "Choose picture source", message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+        let action1:UIAlertAction = UIAlertAction(title: "Choose from library", style: UIAlertActionStyle.Default) { (action) in
+            
+        }
+        action1.setValue(AppTheme.NEVO_SOLAR_YELLOW(), forKey: "titleTextColor")
+        alert.addAction(action1)
+        
+        let action2:UIAlertAction = UIAlertAction(title: "Choose from Camera", style: UIAlertActionStyle.Default) { (action) in
+            
+        }
+        action2.setValue(AppTheme.NEVO_SOLAR_YELLOW(), forKey: "titleTextColor")
+        alert.addAction(action2)
+        
+        let action3:UIAlertAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel) { (action) in
+            
+        }
+        action3.setValue(AppTheme.NEVO_SOLAR_YELLOW(), forKey: "titleTextColor")
+        alert.addAction(action3)
+        viewController()?.presentViewController(alert, animated: true, completion: nil)
+        
+    }
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
