58c629518c31c07b71f94b9f1e2dd07dd478e575
Cloud <546138263@qq.com>
2016-11-01 15:13:36 +0800

    Sync solar data to database

---
+++ Programming/Nevo/Nevo/Class/Global/Int+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Int+Extension.swift
+//  Nevo
+//
+//  Created by Cloud on 2016/11/1.
+//  Copyright © 2016年 Nevo. All rights reserved.
+//
+
+import UIKit
+
+extension Int {
+    func to2String() -> String {
+        let value1:NSString = NSString(format: "%d", self)
+        if value1.length>1 {
+            return value1 as String;
+        }else{
+            return NSString(format: "0%d", self) as String
+        }
+    }
+}
+
