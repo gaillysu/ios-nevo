6f91ea1b95bae2571a8ff6aa3d413073eb71f39c
Quentin <xiangqiu@med-corp.net>
2016-11-04 14:42:52 +0800

    [refactor]: user's part is ok

--- Programming/Nevo/Nevo/Class/Controller/LoginController/LoginController.swift
+++ Programming/Nevo/Nevo/Class/Controller/LoginController/LoginController.swift
@@ -23,9 +23,7 @@ class LoginController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var logoinButton: UIButton!
     @IBOutlet weak var registerLabel: ActiveLabel!
     @IBOutlet weak var platformLabel: UILabel!
-    
-//    fileprivate var backButton:UIButton?
-    
+ 
     var userName:String = ""
     var password:String = ""
     fileprivate var pErrorNumber:Int = 0
@@ -37,8 +35,12 @@ class LoginController: UIViewController,UITextFieldDelegate {
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+}
 
-
+// MARK: - View's Lifecycle
+extension LoginController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -49,14 +51,6 @@ class LoginController: UIViewController,UITextFieldDelegate {
         let leftButton:UIBarButtonItem = UIBarButtonItem(image: UIImage(named: "cancel_lunar"), style: UIBarButtonItemStyle.plain, target: self, action: #selector(backButtonClick(_:)))
         self.navigationItem.leftBarButtonItem = leftButton
         
-        var targetName:String = ""
-        
-        if AppTheme.isTargetLunaR_OR_Nevo() {
-            targetName = "Nevo"
-        } else {
-            targetName = "Lunar"
-        }
-        
         let judgeRootViewController = UIApplication.shared.keyWindow?.rootViewController?.isKind(of: UITabBarController.classForCoder())
         
         if judgeRootViewController! {
@@ -64,12 +58,6 @@ class LoginController: UIViewController,UITextFieldDelegate {
         } else {
             self.navigationItem.leftBarButtonItem = nil
         }
-        
-//        for controllers:UIViewController in self.navigationController!.viewControllers {
-//            if controllers.isKind(of: SetingViewController.self) {
-//                self.navigationItem.rightBarButtonItem = nil
-//            }
-//        }
         
         let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapAction(_:)))
         registerLabel.addGestureRecognizer(tap)
@@ -87,7 +75,7 @@ class LoginController: UIViewController,UITextFieldDelegate {
             passwordTextField.backgroundColor = UIColor.getLightBaseColor()
             userNameTextField.setValue(UIColor.white, forKeyPath: "placeholderLabel.textColor")
             passwordTextField.setValue(UIColor.white, forKeyPath: "placeholderLabel.textColor")
-
+            
             registerLabel.textColor = UIColor.getBaseColor()
             
             userNameTextField.textColor = UIColor.white
@@ -105,24 +93,14 @@ class LoginController: UIViewController,UITextFieldDelegate {
         
         navigationController?.navigationBar.subviewsSatisfy(theCondition: { (v) -> (Bool) in
             return v.frame.height == 0.5
-            }, do: { (v) in
-                v.isHidden = true
+        }, do: { (v) in
+            v.isHidden = true
         })
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let user:NSArray = UserProfile.getAll()
-        if user.count>0 {
-            let judgeRootViewController = UIApplication.shared.keyWindow?.rootViewController?.isKind(of: UITabBarController.classForCoder())
-            
-            if judgeRootViewController! {
-                self.dismiss(animated: true, completion: nil)
-            } else {
-                UIApplication.shared.keyWindow?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
-            }
-        }
-        
+
         if let email_pw = UserDefaults.standard.object(forKey: InformationController.MED_kISFromRegisterController) as? [String : String] {
             userNameTextField.text = email_pw["email"]!
             passwordTextField.text = email_pw["password"]!
@@ -132,22 +110,30 @@ class LoginController: UIViewController,UITextFieldDelegate {
         }
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+//        self.userNameTextField.text = ""
+        // when back from the forgot-password controller, the passwordTextField is supposed to be empty. though, if want the field be filled with the new password, it would be ok.
+        self.passwordTextField.text = ""
+    }
+}
+
+
+// MARK: - Touch Event
+extension LoginController {
+    
     func tapAction(_ sender:UITapGestureRecognizer) {
         let register:ProfileSetupViewController = ProfileSetupViewController()
         let naviController:UINavigationController = UINavigationController(rootViewController: register)
         
         self.present(naviController, animated: true, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
     
     func rightAction(_ sender:UIBarButtonItem) {
         let register:ProfileSetupViewController = ProfileSetupViewController()
         self.navigationController?.pushViewController(register, animated: true)
     }
-
+    
     @IBAction func buttonAction(_ sender: AnyObject) {
         if sender.isEqual(logoinButton) {
             self.loginRequest()
@@ -161,15 +147,28 @@ class LoginController: UIViewController,UITextFieldDelegate {
         DispatchQueue.main.asyncAfter(
             deadline: DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: closure)
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
     
+    @IBAction func skipButtonClick(_ sender: AnyObject) {
+        let naviController:UINavigationController = UINavigationController(rootViewController: TutorialOneViewController())
+        naviController.isNavigationBarHidden = true
+        
+        UIApplication.shared.keyWindow?.rootViewController = naviController
+    }
     
+    @objc fileprivate func backButtonClick(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+// MAKR: - Network
+extension LoginController {
     func loginRequest() {
-            userName = userNameTextField!.text!
-            password = passwordTextField!.text!
+        userName = userNameTextField!.text!
+        password = passwordTextField!.text!
         if AppDelegate.getAppDelegate().network!.isReachable {
             XCGLogger.default.debug("有网络")
             if(AppTheme.isNull(userName) || AppTheme.isEmail(userName)) {
@@ -188,7 +187,7 @@ class LoginController: UIViewController,UITextFieldDelegate {
             
             let view = MRProgressOverlayView.showOverlayAdded(to: self.navigationController!.view, title: NSLocalizedString("please_wait", comment: ""), mode: MRProgressOverlayViewMode.indeterminate, animated: true)
             view?.setTintColor(AppTheme.NEVO_SOLAR_YELLOW())
-
+            
             if !AppTheme.isTargetLunaR_OR_Nevo() {
                 view?.setTintColor(UIColor.getBaseColor())
             }
@@ -221,7 +220,7 @@ class LoginController: UIViewController,UITextFieldDelegate {
                             milesValue = miles
                         })
                         
-//                        let value:[String:Any] = ["steps":["uid":profile.id,"steps":stepsModel.hourlysteps,"date":dateString,"calories":caloriesValue,"active_time":stepsModel.walking_duration+stepsModel.running_duration,"distance":milesValue]]
+                        //                        let value:[String:Any] = ["steps":["uid":profile.id,"steps":stepsModel.hourlysteps,"date":dateString,"calories":caloriesValue,"active_time":stepsModel.walking_duration+stepsModel.running_duration,"distance":milesValue]]
                         let activeTime: Int = stepsModel.walking_duration+stepsModel.running_duration
                         MEDStepsNetworkManager.createSteps(uid: profile.id, steps: stepsModel.hourlysteps, date: dateString, activeTime: activeTime, calories: caloriesValue, distance: milesValue, completion: { (success: Bool) in
                             if success {
@@ -230,7 +229,7 @@ class LoginController: UIViewController,UITextFieldDelegate {
                             }
                         })
                     }
-
+                    
                     for sleepValue in sleepArray {
                         let sleepModel:UserSleep = sleepValue as! UserSleep
                         let profile:UserProfile = user!
@@ -265,7 +264,7 @@ class LoginController: UIViewController,UITextFieldDelegate {
                             let nav:UINavigationController = UINavigationController(rootViewController: forget)
                             self.present(nav, animated: true, completion: nil)
                         })
-            
+                        
                         forgetPassword.addAction(alertAction)
                         
                         let alertAction2:UIAlertAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: UIAlertActionStyle.cancel, handler: { (action) in
@@ -282,21 +281,9 @@ class LoginController: UIViewController,UITextFieldDelegate {
             XCGLogger.default.debug("没有网络")
             let view = MRProgressOverlayView.showOverlayAdded(to: self.navigationController!.view, title: "No internet", mode: MRProgressOverlayViewMode.cross, animated: true)
             view?.setTintColor(UIColor.getBaseColor())
-            Timer.after(1, { 
+            Timer.after(1, {
                 MRProgressOverlayView.dismissAllOverlays(for: self.navigationController!.view, animated: true)
             })
         }
-        
-    }
-
-    @IBAction func skipButtonClick(_ sender: AnyObject) {
-        let naviController:UINavigationController = UINavigationController(rootViewController: TutorialOneViewController())
-        naviController.isNavigationBarHidden = true
-        
-        UIApplication.shared.keyWindow?.rootViewController = naviController
-    }
-    
-    @objc fileprivate func backButtonClick(_ sender: AnyObject) {
-        self.dismiss(animated: true, completion: nil)
     }
 }
