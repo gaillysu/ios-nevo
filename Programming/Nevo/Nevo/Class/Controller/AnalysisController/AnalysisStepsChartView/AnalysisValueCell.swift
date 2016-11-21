234e406d61099fa547e5856102b285967ec05d05
Quentin <xiangqiu@med-corp.net>
2016-11-21 15:06:10 +0800

    [optimize]: the data label's appearance on analysis view

--- Programming/Nevo/Nevo/Class/Controller/AnalysisController/AnalysisStepsChartView/AnalysisValueCell.swift
+++ Programming/Nevo/Nevo/Class/Controller/AnalysisController/AnalysisStepsChartView/AnalysisValueCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class AnalysisValueCell: UICollectionViewCell {
     
-    @IBOutlet weak var labelWidthConstraint: NSLayoutConstraint!
+//    @IBOutlet weak var labelWidthConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var valueLabel: UILabel!
@@ -22,6 +22,7 @@ class AnalysisValueCell: UICollectionViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
         if !AppTheme.isTargetLunaR_OR_Nevo() {
             titleLabel.textColor = UIColor.white
             valueLabel.textColor = UIColor.getBaseColor()
@@ -31,12 +32,12 @@ class AnalysisValueCell: UICollectionViewCell {
     func updateTitleLabel(_ labelText: String){
         let contentDict:[String:AnyObject] = [NSFontAttributeName:titleLabel.font]
         titleLabel.text = labelText
-        let statusLabelSize = labelText.size(attributes: contentDict)
-        if statusLabelSize.width>85 {
-            labelWidthConstraint.constant = statusLabelSize.width+5;
-        }else{
-            labelWidthConstraint.constant = 85
-        }
+//        let statusLabelSize = labelText.size(attributes: contentDict)
+//        if statusLabelSize.width>85 {
+//            labelWidthConstraint.constant = statusLabelSize.width+5;
+//        }else{
+//            labelWidthConstraint.constant = 85
+//        }
         
         layoutIfNeeded()
     }
