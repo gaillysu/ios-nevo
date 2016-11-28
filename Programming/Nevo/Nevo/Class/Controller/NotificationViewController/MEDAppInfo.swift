71be02ef21a6df87d90e262c59b3ab0cf64c0889
Quentin <xiangqiu@med-corp.net>
2016-11-28 11:19:44 +0800

    [add]: request files

---
+++ Programming/Nevo/Nevo/Class/Controller/NotificationViewController/MEDAppInfo.swift
@@ -0,0 +1,18 @@
+//
+//  MEDAppInfo.swift
+//  Nevo
+//
+//  Created by Quentin on 28/11/16.
+//  Copyright © 2016年 Nevo. All rights reserved.
+//
+
+import Foundation
+
+class MEDAppInfo {
+    var resultCount = 1
+    var iconSmall = ""
+    var iconNormal = ""
+    var iconLarge = ""
+    var bundleID = ""
+
+}
---
+++ Programming/Nevo/Nevo/Class/Controller/NotificationViewController/MEDAppInfoRequester.swift
@@ -0,0 +1,15 @@
+//
+//  MEDAppInfoRequester.swift
+//  Nevo
+//
+//  Created by Quentin on 28/11/16.
+//  Copyright © 2016年 Nevo. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class MEDAppInfoRequester {
+    let baseURL = "https://itunes.apple.com/lookup?bundleId="
+}
